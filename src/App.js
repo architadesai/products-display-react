ffacf405d8b11550db7fb5d0aaf30518c1ffb9f8
Archita Desai <architad8@gmail.com>
2019-07-19 14:27:07 +0530

    Fetch Amazon details from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            products: {},
+            loading: false 
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            loading: true
+        });
+        
+        const url = 'https://test.pickcel.com/api/v1/getAmazonDeals';
+        fetch(url)
+            .then((response) => response.json())
+                .then((data) => {
+                    this.setState({
+                        loading: false,
+                        products: data
+                    })
+                })
+
+    }
+
+    render() {
+        const data = this.state.loading ? "Loading" : this.state.products;
+        console.log(data)
+        return (
+            <h1>data</h1>
+        );
+    }
 }
+
 
 export default App;
